4c989442893b7ba6f1b487f770f6222675916c44
lucio-araujo <lucio.ribeiroaraujo@outlook.com>
2023-11-13 16:02:59 -0300

    Sign-in com github OAuth

--- src/components/SigninButton/index.tsx
+++ src/components/SigninButton/index.tsx
@@ -1,17 +1,32 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
-  const isActive = true;
-  const username = "Lúcio Araújo";
+  const { data: session } = useSession();
 
-  return (
-    <button className={styles.container}>
-      <FaGithub color={isActive ? "#04d361" : "#EBA417"} />
-      {isActive ? username : "Sign in with GitHub"}
-      {isActive && <FiX color="737380" className={styles.closeIcon} />}
+  console.log(session);
+
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
+      <FaGithub color="#04d361" />
+      {session.user?.name}
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
+      <FaGithub color="#eba417" />
+      Sign in with Github
     </button>
   );
 }
