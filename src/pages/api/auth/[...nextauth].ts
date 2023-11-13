61439badf160e110ae4559a9e5f75b1440547cf7
lucio-araujo <lucio.ribeiroaraujo@outlook.com>
2023-11-13 18:53:37 -0300

    Inserindo usuário no BD

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,33 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
+import { query as q } from "faunadb";
+
+import { fauna } from "../../../services/fauna";
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      scope: ['read:user'],
+      scope: ["read:user"],
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      try {
+        await fauna.query(
+          q.Create(q.Collection("users"), {
+            data: {
+              email: user.email,
+            },
+          })
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
