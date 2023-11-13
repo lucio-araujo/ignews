b58887e555a1ed529341f93150d57729d9e97d19
lucio-araujo <lucio.ribeiroaraujo@outlook.com>
2023-11-13 19:21:13 -0300

    Verificando se user existe

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -20,14 +20,21 @@ export default NextAuth({
   ],
   callbacks: {
     async signIn(user, account, profile) {
+      const { email } = user;
+
       try {
         await fauna.query(
-          q.Create(q.Collection("users"), {
-            data: {
-              email: user.email,
-            },
-          })
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
         );
+
         return true;
       } catch {
         return false;
