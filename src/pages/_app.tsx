4cb87ddf3123c4209bcbccb132bb06de3c82fe25
lucio-araujo <lucio.ribeiroaraujo@outlook.com>
2023-12-11 11:34:20 -0300

    fix: pageProps.session

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,14 @@ import { Header } from "../components/Header";
 import { Provider as NextAuthProvider } from "next-auth/client";
 
 import "../styles/global.scss";
+import { Session } from "next-auth";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) {
   return (
     <NextAuthProvider session={pageProps.session}>
       <Header />
